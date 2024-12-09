e62ec8a5ffed79481b63d1a64e2a474f2a618aed
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-12-09 22:33:41 +0530

    Telegram Bot implemented

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hlel");
+import "dotenv/config";
+import { Telegraf } from "telegraf";
+
+const bot = new Telegraf(process.env.BOT_TOKEN || "");
+
+bot.start((ctx) => {
+    ctx.reply(
+        "Welcome to the SolanaGuideBot! Use /help to see available commands."
+    );
+});
+
+bot.help((ctx) => {
+    ctx.reply(
+        "Commands:\n/balance <address> - Check SOL balance\n/history <address> - View recent transactions\n/airdrop <address> - Get free SOL (Devnet only)"
+    );
+});
+
+bot.launch();
+console.log("Bot is running...");
